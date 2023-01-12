53cc10da1815a806709eb3c0ab37bbd3c45bbc00
Liberty Mugwara <libertymugwara@live.com>
2023-01-12 13:06:40 +0200

    play: dynamodb scan

--- nodejs/node18/src/GetGroups.ts
+++ nodejs/node18/src/GetGroups.ts
@@ -1,1 +1,40 @@
-export {};
+import { DynamoDBDocumentClient, ScanCommand } from "@aws-sdk/lib-dynamodb";
+
+import { DynamoDBClient } from "@aws-sdk/client-dynamodb";
+
+const ddbClient = new DynamoDBClient({ region: "us-east-1" });
+
+const marshallOptions = {
+  // Whether to automatically convert empty strings, blobs, and sets to `null`.
+  convertEmptyValues: false, // false, by default.
+  // Whether to remove undefined values while marshalling.
+  removeUndefinedValues: true, // false, by default.
+  // Whether to convert typeof object to map attribute.
+  convertClassInstanceToMap: false, // false, by default.
+};
+
+const unmarshallOptions = {
+  // Whether to return numbers as a string instead of converting them to native JavaScript numbers.
+  wrapNumbers: false, // false, by default.
+};
+
+const ddbDocumentClient = DynamoDBDocumentClient.from(ddbClient, {
+  marshallOptions,
+  unmarshallOptions,
+});
+
+export const handler = async (event: any) => {
+  const scanParams = {
+    TableName: "Groups",
+  };
+
+  const res = await ddbDocumentClient.send(new ScanCommand(scanParams));
+
+  return {
+    statusCode: 200,
+    headers: {
+      "Access-Control-Allow-Origin": "*",
+    },
+    body: JSON.stringify({ items: res.Items || [] }),
+  };
+};
